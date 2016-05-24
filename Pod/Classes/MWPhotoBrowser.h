0087ddff6dd85abe47b91b425bbf02f67beb759b
luchi0208 <luanpro23@gmail.com>
2016-05-24 15:25:47 +1200

    Add files via upload

---
+++ Pod/Classes/MWPhotoBrowser.h
@@ -0,0 +1,197 @@
+//
+//  MWPhotoBrowser.h
+//  MWPhotoBrowser
+//
+//  Created by Michael Waterfall on 14/10/2010.
+//  Copyright 2010 d3i. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MWPhoto.h"
+#import "MWPhotoProtocol.h"
+#import "MWCaptionView.h"
+#import <MBProgressHUD/MBProgressHUD.h>
+#import <MediaPlayer/MediaPlayer.h>
+#import "MWGridViewController.h"
+#import "MWZoomingScrollView.h"
+// Debug Logging
+#if 0 // Set to 1 to enable debug logging
+#define MWLog(x, ...) NSLog(x, ## __VA_ARGS__);
+#else
+#define MWLog(x, ...)
+#endif
+
+@class MWPhotoBrowser;
+
+@protocol MWPhotoBrowserDelegate <NSObject>
+
+- (NSUInteger)numberOfPhotosInPhotoBrowser:(MWPhotoBrowser *)photoBrowser;
+- (id <MWPhoto>)photoBrowser:(MWPhotoBrowser *)photoBrowser photoAtIndex:(NSUInteger)index;
+
+@optional
+
+- (id <MWPhoto>)photoBrowser:(MWPhotoBrowser *)photoBrowser thumbPhotoAtIndex:(NSUInteger)index;
+- (MWCaptionView *)photoBrowser:(MWPhotoBrowser *)photoBrowser captionViewForPhotoAtIndex:(NSUInteger)index;
+- (NSString *)photoBrowser:(MWPhotoBrowser *)photoBrowser titleForPhotoAtIndex:(NSUInteger)index;
+- (void)photoBrowser:(MWPhotoBrowser *)photoBrowser didDisplayPhotoAtIndex:(NSUInteger)index;
+- (void)photoBrowser:(MWPhotoBrowser *)photoBrowser actionButtonPressedForPhotoAtIndex:(NSUInteger)index;
+- (BOOL)photoBrowser:(MWPhotoBrowser *)photoBrowser isPhotoSelectedAtIndex:(NSUInteger)index;
+- (void)photoBrowser:(MWPhotoBrowser *)photoBrowser photoAtIndex:(NSUInteger)index selectedChanged:(BOOL)selected;
+- (void)photoBrowserDidFinishModalPresentation:(MWPhotoBrowser *)photoBrowser;
+
+@end
+
+@interface MWPhotoBrowser : UIViewController <UIScrollViewDelegate, UIActionSheetDelegate>{
+    
+    
+    // Data
+    NSUInteger _photoCount;
+    NSMutableArray *_photos;
+    NSMutableArray *_thumbPhotos;
+    NSArray *_fixedPhotosArray; // Provided via init
+    
+    // Views
+    UIScrollView *_pagingScrollView;
+    
+    // Paging & layout
+    NSMutableSet *_visiblePages, *_recycledPages;
+    NSUInteger _currentPageIndex;
+    NSUInteger _previousPageIndex;
+    CGRect _previousLayoutBounds;
+    NSUInteger _pageIndexBeforeRotation;
+    
+    // Navigation & controls
+    UIToolbar *_toolbar;
+    NSTimer *_controlVisibilityTimer;
+    UIBarButtonItem *_previousButton, *_nextButton, *_actionButton, *_doneButton;
+    MBProgressHUD *_progressHUD;
+    
+    // Grid
+    MWGridViewController *_gridController;
+    UIBarButtonItem *_gridPreviousLeftNavItem;
+    UIBarButtonItem *_gridPreviousRightNavItem;
+    
+    // Appearance
+    BOOL _previousNavBarHidden;
+    BOOL _previousNavBarTranslucent;
+    UIBarStyle _previousNavBarStyle;
+    UIStatusBarStyle _previousStatusBarStyle;
+    UIColor *_previousNavBarTintColor;
+    UIColor *_previousNavBarBarTintColor;
+    UIBarButtonItem *_previousViewControllerBackButton;
+    UIImage *_previousNavigationBarBackgroundImageDefault;
+    UIImage *_previousNavigationBarBackgroundImageLandscapePhone;
+    
+    // Video
+    MPMoviePlayerViewController *_currentVideoPlayerViewController;
+    NSUInteger _currentVideoIndex;
+    UIActivityIndicatorView *_currentVideoLoadingIndicator;
+    
+    // Misc
+    BOOL _hasBelongedToViewController;
+    BOOL _isVCBasedStatusBarAppearance;
+    BOOL _statusBarShouldBeHidden;
+    BOOL _displayActionButton;
+    BOOL _leaveStatusBarAlone;
+    BOOL _performingLayout;
+    BOOL _rotating;
+    BOOL _viewIsActive; // active as in it's in the view heirarchy
+    BOOL _didSavePreviousStateOfNavBar;
+    BOOL _skipNextPagingScrollViewPositioning;
+    BOOL _viewHasAppearedInitially;
+    CGPoint _currentGridContentOffset;
+    
+
+}
+
+@property (nonatomic, weak) IBOutlet id<MWPhotoBrowserDelegate> delegate;
+@property (nonatomic) BOOL zoomPhotosToFill;
+@property (nonatomic) BOOL displayNavArrows;
+@property (nonatomic) BOOL displayActionButton;
+@property (nonatomic) BOOL displaySelectionButtons;
+@property (nonatomic) BOOL alwaysShowControls;
+@property (nonatomic) BOOL enableGrid;
+@property (nonatomic) BOOL enableSwipeToDismiss;
+@property (nonatomic) BOOL startOnGrid;
+@property (nonatomic) BOOL autoPlayOnAppear;
+@property (nonatomic) NSUInteger delayToHideElements;
+@property (nonatomic, readonly) NSUInteger currentIndex;
+
+// Properties
+@property (nonatomic) UIActivityViewController *activityViewController;
+
+// Layout
+- (void)layoutVisiblePages;
+- (void)performLayout;
+- (BOOL)presentingViewControllerPrefersStatusBarHidden;
+
+// Nav Bar Appearance
+- (void)setNavBarAppearance:(BOOL)animated;
+- (void)storePreviousNavBarAppearance;
+- (void)restorePreviousNavBarAppearance:(BOOL)animated;
+
+// Paging
+- (void)tilePages;
+- (BOOL)isDisplayingPageForIndex:(NSUInteger)index;
+- (MWZoomingScrollView *)pageDisplayedAtIndex:(NSUInteger)index;
+- (MWZoomingScrollView *)pageDisplayingPhoto:(id<MWPhoto>)photo;
+- (MWZoomingScrollView *)dequeueRecycledPage;
+- (void)configurePage:(MWZoomingScrollView *)page forIndex:(NSUInteger)index;
+- (void)didStartViewingPageAtIndex:(NSUInteger)index;
+
+// Frames
+- (CGRect)frameForPagingScrollView;
+- (CGRect)frameForPageAtIndex:(NSUInteger)index;
+- (CGSize)contentSizeForPagingScrollView;
+- (CGPoint)contentOffsetForPageAtIndex:(NSUInteger)index;
+- (CGRect)frameForToolbarAtOrientation:(UIInterfaceOrientation)orientation;
+- (CGRect)frameForCaptionView:(MWCaptionView *)captionView atIndex:(NSUInteger)index;
+- (CGRect)frameForSelectedButton:(UIButton *)selectedButton atIndex:(NSUInteger)index;
+
+// Navigation
+- (void)updateNavigation;
+- (void)jumpToPageAtIndex:(NSUInteger)index animated:(BOOL)animated;
+- (void)gotoPreviousPage;
+- (void)gotoNextPage;
+
+// Grid
+- (void)showGrid:(BOOL)animated;
+- (void)hideGrid;
+
+// Controls
+- (void)cancelControlHiding;
+- (void)hideControlsAfterDelay;
+- (void)setControlsHidden:(BOOL)hidden animated:(BOOL)animated permanent:(BOOL)permanent;
+- (void)toggleControls;
+- (BOOL)areControlsHidden;
+
+// Data
+- (NSUInteger)numberOfPhotos;
+- (id<MWPhoto>)photoAtIndex:(NSUInteger)index;
+- (id<MWPhoto>)thumbPhotoAtIndex:(NSUInteger)index;
+- (UIImage *)imageForPhoto:(id<MWPhoto>)photo;
+- (BOOL)photoIsSelectedAtIndex:(NSUInteger)index;
+- (void)setPhotoSelected:(BOOL)selected atIndex:(NSUInteger)index;
+- (void)loadAdjacentPhotosIfNecessary:(id<MWPhoto>)photo;
+- (void)releaseAllUnderlyingPhotos:(BOOL)preserveCurrent;
+
+// Customise image selection icons as they are the only icons with a colour tint
+// Icon should be located in the app's main bundle
+@property (nonatomic, strong) NSString *customImageSelectedIconName;
+@property (nonatomic, strong) NSString *customImageSelectedSmallIconName;
+
+// Init
+- (id)initWithPhotos:(NSArray *)photosArray;
+- (id)initWithDelegate:(id <MWPhotoBrowserDelegate>)delegate;
+
+// Reloads the photo browser and refetches data
+- (void)reloadData;
+
+// Set page that photo browser starts on
+- (void)setCurrentPhotoIndex:(NSUInteger)index;
+
+// Navigation
+- (void)showNextPhotoAnimated:(BOOL)animated;
+- (void)showPreviousPhotoAnimated:(BOOL)animated;
+
+@end
